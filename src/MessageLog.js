018921e2691a364f1985b6689568d110b1a6e543
root <Ritesh-Kumawat>
2019-05-22 13:32:17 +0000

    First commit

---
+++ src/MessageLog.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MessageLog = props => {
+  const { messages, user } = props;
+
+  return (
+    <ul className="message-list">
+      {messages.map((message, index) => (
+        <li
+          key={index}
+          className={message.username === user.username ? 'message sender' : 'message recipient'}
+        >
+          <p>{`${message.username}: ${message.text}`}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+MessageLog.propTypes = {
+  messages: PropTypes.array.isRequired,
+  user: PropTypes.object.isRequired,
+};
+
+export default MessageLog;
